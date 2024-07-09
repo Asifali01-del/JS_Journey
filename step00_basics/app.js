a40d176b698ae43f63391cd0b1b78e73c60e3dc0
Asif Ali Arain <casif7823@gmail.com>
2024-07-09 09:08:46 +0500

    Add files via upload

---
+++ step00_basics/app.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+let myName = "Asif";
+console.log(myName);
+var message = "hello world";
+console.log(message);
---
+++ step00_basics/app.ts
@@ -0,0 +1,8 @@
+console.log("hello world");
+
+let myName = "Asif";
+console.log(myName);
+
+let message = "hello world";
+console.log(message);
+
