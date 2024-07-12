1560d00e2fea2b30335b2c546707cea61c6f82b5
Asif Ali Arain <casif7823@gmail.com>
2024-07-12 16:32:05 +0500

    Add files via upload

---
+++ strep01_variables/main.js
@@ -0,0 +1,15 @@
+var message = "hello world";
+message = "asif";
+console.log(message);
+// value of let can be change
+var userName = "asif"; // but in const value don't change
+// userName = "Asif"
+console.log(userName);
+// use of var is very old so we use let instead of var
+// json object
+var profile = {
+    Name1: "Asif",
+    Age: 19,
+    isStudent: true
+};
+console.log(profile);
---
+++ strep01_variables/main.ts
@@ -0,0 +1,22 @@
+let message = "hello world"
+message = "asif"
+console.log(message);
+
+// value of let can be change
+
+const userName = "asif"  // but in const value don't change
+// userName = "Asif"
+console.log(userName);
+
+// use of var is very old so we use let instead of var
+
+
+let profile = {
+Name1: "Asif",
+Age: 19 ,
+isStudent: true
+
+}
+
+console.log(profile);
+
